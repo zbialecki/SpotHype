1023be5879aabcc1f7bf327528f221238b4b97ee
Zach Bialecki <zach.bialecki@gmail.com>
2014-07-17 16:21:33 -0400

    Add sample config file

---
+++ config.sample.js
@@ -0,0 +1,14 @@
+/**
+ * Private Config Variables
+ */
+
+var config = {};
+
+config.spotify = {
+	clientId: 'spotifyclientidgoeshere',
+	clientSecret: 'secretkeygoeshere'
+};
+
+config.hypem = {};
+
+module.exports = config;
